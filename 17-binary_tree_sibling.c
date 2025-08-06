f52fc4605aa39a617e636346f8393030f5435d00
h7k2 <keddousheytem@gmail.com>
2025-08-06 22:51:35 +0200

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,11 +11,13 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
-		return (node->parent->right);
-
-	if (node->parent->right == node)
-		return (node->parent->left);
+	if (node->parent->left && node->parent->right)
+	{
+		if (node == node->parent->left)
+			return (node->parent->right);
+		else if (node == node->parent->right)
+			return (node->parent->left);
+	}
 
 	return (NULL);
 }
